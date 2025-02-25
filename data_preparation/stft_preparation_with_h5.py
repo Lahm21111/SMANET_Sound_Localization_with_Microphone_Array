9869548df84a1298af9cdf64cc8c790928bce9a2
Ziang Liu <liuziang_2023@163.com>
2025-02-25 13:45:55 +0100

    test the feasibility of the model in the custom dataset

--- data_preparation/stft_preparation_with_h5.py
+++ data_preparation/stft_preparation_with_h5.py
@@ -3,7 +3,7 @@ import numpy as np
 import librosa
 
 # Path to the H5 file
-h5_file_path = r"C:\Users\grizi\Desktop\TUD\year2\thesis\neural_network\DoA_Net\data\training_more.h5"
+h5_file_path = r"C:\Users\grizi\Desktop\TUD\year2\thesis\neural_network\dataset_tools\car_test_dataset.h5"
 
 # Open the H5 file
 with h5py.File(h5_file_path, 'r') as h5file:
@@ -15,6 +15,8 @@ with h5py.File(h5_file_path, 'r') as h5file:
             audio_data = np.array(dataset)
         elif dataset_name == 'label':
             label_data = np.array(dataset)
+        elif dataset_name == 'sound_data':
+            audio_data = np.array(dataset)
         
         # Print the dataset name and its shape
         print(f"Dataset: {dataset_name}, Shape: {dataset.shape}")
@@ -22,8 +24,8 @@ with h5py.File(h5_file_path, 'r') as h5file:
 def process_audio(audio):
 
     # set the parameter for stft processing 
-    sr =  24000
-    num_mics = 4  # Four microphones
+    sr =  48000
+    num_mics = 32  # Four microphones
     num_segments = len(audio)
     frame_size = 512  # time window
     hop_size = frame_size // 2  # 50% overlap
@@ -34,11 +36,10 @@ def process_audio(audio):
     print(valid_bins)
 
     # Storage for all segments
-    output_data = np.zeros((num_segments, num_mics * 2, 168, 7), dtype=np.float32)
+    output_data = np.zeros((num_segments, num_mics * 2, 84, 15), dtype=np.float32)
 
     # Process each segment separately
     for i in range(len(audio)):
-        print(i)
         segment = audio[i,:].T
         print(segment.shape,frame_size,hop_size)
 
@@ -47,7 +48,7 @@ def process_audio(audio):
         print(stfts.shape)
 
         # Keep only the desired frequency bins
-        stfts = stfts[:, valid_bins, :7]  # Ensure exactly 7 time frames
+        stfts = stfts[:, valid_bins, :15]  # Ensure exactly 7 time frames
         print(stfts.shape)
 
         # Separate real and imaginary parts
@@ -60,17 +61,20 @@ def process_audio(audio):
 
     return output_data
 
+audio_data = audio_data.reshape((1,audio_data.shape[0],audio_data.shape[1]))
+print("the shape of the audio_data is ", audio_data.shape)
 stfts = process_audio(audio_data)
+print(stfts.shape)
 
 # save the stft data to h5 file 
 # with h5py.File(h5_file_path, 'a') as h5file:
 #     h5file.create_dataset('stft', data=stfts)
 
-stft_h5 = r"C:\Users\grizi\Desktop\TUD\year2\thesis\neural_network\DoA_Net\data\stft.h5"
+# stft_h5 = r"C:\Users\grizi\Desktop\TUD\year2\thesis\neural_network\DoA_Net\data\stft.h5"
 
-with h5py.File(stft_h5, 'w') as h5file1:
-    h5file1.create_dataset('stft', data=stfts)
-    h5file1.create_dataset('audio', data=audio_data)
-    h5file1.create_dataset('label', data=label_data)
+# with h5py.File(stft_h5, 'w') as h5file1:
+#     h5file1.create_dataset('stft', data=stfts)
+#     h5file1.create_dataset('audio', data=audio_data)
+#     h5file1.create_dataset('label', data=label_data)
 
-print("Processing complete. Data shape:", stfts.shape)
+# print("Processing complete. Data shape:", stfts.shape)
